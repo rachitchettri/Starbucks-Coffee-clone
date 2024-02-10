4a0fb0fd6e6f5842d32f1814925704c2f7479626
rachitchettri <rachitchettri7@gmail.com>
2024-02-10 21:11:00 +0545

    ..

--- src/assets/component/Hero.jsx
+++ src/assets/component/Hero.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Hero.css";
+import Hero1 from "./hero1.png";
 const Hero = () => {
   return (
+    <div>
+    <div className="hero-left">
     <div className="heroimg">
-      <img src="" alt="" />
+      <img src={Hero1} alt="" />
+    </div>
+    </div>
+    <div className="hero-right">
+
+    </div>
     </div>
   );
 };
