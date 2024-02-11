7cf48829bd48bc91b9739705aafc9cda42722064
rachitchettri <rachitchettri7@gmail.com>
2024-02-11 08:18:22 +0545

    30%

--- src/assets/component/Hero.jsx
+++ src/assets/component/Hero.jsx
@@ -3,16 +3,20 @@ import "./Hero.css";
 import Hero1 from "./hero1.png";
 const Hero = () => {
   return (
-    <div1>
-
     
+
+  <div className="container">
     <div className="heroimg">
       <img src={Hero1} alt="" />
     </div>
     <div className="hero-right">
-      ssd
+    <h2>You + Starbucks Rewards = ❤️</h2>
+    <div className="para">
+      <p>Sounds like a perfect match. Everything you need is in the app: download and start earning free drinks and more. Plus, enjoy mobile ordering and offers made just for you.</p>
+      <button class="btn">Get to app</button>
     </div>
-    </div1>
+      </div>
+</div>
   );
 };
 
