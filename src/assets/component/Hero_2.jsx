f748035e3e86f3ca6f2afda480d8d4132676451f
rachitchettri <rachitchettri7@gmail.com>
2024-02-13 15:53:53 +0545

    nnn

--- src/assets/component/Hero_2.jsx
+++ src/assets/component/Hero_2.jsx
@@ -6,7 +6,7 @@ const Hero_2 = () => {
   return (
     <div className="container_2">
         <div className="left_2">
-            <h2>What the heart wants</h2>
+            <h1>What the heart wants</h1>
             <div className="para_2">
                 <p>Treat yourself and a friend to swirls of happiness and rich java chips with the new Chocolate-Covered Strawberry Crème Frappuccino® drink. Here for a limited time.</p>
                 <button class="btn_2">Order Now</button>
